909720026285d19cc96ac11d4a923cdb5d188e2e
Kudzanayi Dzvairo <kdzvairo@gmail.com>
2018-10-31 02:30:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ var kittens = ['Milo','Otis','Garfield'] //define your array here
 
 // Add your functions and code here
 function destructivelyAppendKitten(name) {
-    kittens.push(name) = a
-    return a
+    kittens.push(name)
+    return kittens
   }
